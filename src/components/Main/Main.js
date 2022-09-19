b0ff968e59d1d129689b82a4dd2aecd58a40f2ed
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-19 14:17:08 -0700

    main.js useBlogs.js fetching for render

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import { useBlogs } from '../../hooks/useBlogs';
+import BlogCard from '../BlogCard/BlogCard';
+
+export default function Main() {
+
+  const blogs = useBlogs();
+  return (
+    <main>
+      {blogs.map((blog) => {
+        <BlogCard key={blog.id} {...blog} />;
+      })}
+    ;
+    </main>
+  );
+
+}
---
+++ src/hooks/useBlogs.js
@@ -0,0 +1,17 @@
+// useEffect useState getBlogs
+
+import { useEffect, useState } from 'react';
+import { getBlogs } from '../services/blogs';
+
+export function useBlogs() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchBlogs() {
+      const data = await getBlogs();
+      setBlogs(data);
+    }
+    fetchBlogs();
+  }, []);
+  return blogs;
+}
