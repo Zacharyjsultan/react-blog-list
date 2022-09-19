6c7a2b13cc1b6b006720cdae51ec710e33f0f55f
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-19 14:23:41 -0700

    fixes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,12 +5,11 @@ export default function Main() {
 
   const blogs = useBlogs();
   return (
-    <main>
-      {blogs.map((blog) => {
-        <BlogCard key={blog.id} {...blog} />;
-      })}
-    ;
-    </main>
+    <div>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </div>
   );
 
 }
